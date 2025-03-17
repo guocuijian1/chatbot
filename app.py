5efc711cde701f2b6f46fd3cbe6d7d17a6a97c81
CGuo <cguo2@opentext.com>
2025-03-17 15:25:07 +0800

    second commit

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+from flask_assets import Environment
+from webassets import Bundle
+
 from chatservice import Chat
 
 app = Flask(__name__)
+# Setup Flask-Assets
+assets = Environment(app)
 
+# SCSS Compilation
+scss = Bundle('scss/style.scss', output='css/style.css', filters='libsass')
+assets.register('scss_all', scss)
+scss.build()  # This compiles SCSS into CSS
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'hello'
+    return render_template('index.html')
 
 
-@app.route('/chat')
+@app.route('/chat', methods=['POST'])
 def chat():  # put application's code here
+    user_message = request.json.get('message')
     ins = Chat.get_instance()
-    return ins.chat('How many products do you have?', [])
+    response = ins.chat(user_message, [])
+    return {'response': response}
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ static/main.js
@@ -0,0 +1,30 @@
+function sendMessage() {
+    let inputField = document.getElementById('textInput');
+    let message = inputField.value.trim();
+    if (message === '') return;
+
+    let chatBox = document.getElementById('chat-box');
+    chatBox.innerHTML += `<div class="message user"><b>You:</b> ${message}</div>`;
+    let original =chatBox.innerHTML;
+    chatBox.innerHTML = original + `<div><span id="bot-input-animation-1">&nbsp;</span>
+    <span id="bot-input-animation-2">&nbsp;</span>
+    <span id="bot-input-animation-3">&nbsp;</span></div>`
+    fetch('/chat', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ message: message })
+    })
+        .then(response => response.json())
+        .then(data => {
+            chatBox.innerHTML = original + `<div class="message bot"><b>Bot:</b> ${data.response}</div>`;
+            chatBox.scrollTop = chatBox.scrollHeight;
+        });
+
+    inputField.value = '';
+}
+
+function onKeyEnter(event) {
+    if (event.key === 'Enter') {
+        sendMessage();
+    }
+}
